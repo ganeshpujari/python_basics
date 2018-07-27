0419814ff106c7fc5657d5860f797f2924c5a033
Ganesh Pujari <ganesh.pujari@CALSOFTHQ.COM>
2018-07-27 10:55:41 +0530

    add one more example for reduce

--- scripts/reduce.py
+++ scripts/reduce.py
@@ -6,3 +6,9 @@
 from functools import reduce
 result=reduce(lambda number1, number2 : number1 + number2, [1, 2, 3, 4, 5 ])
 print(result)
+
+def addition(number1, number2):
+    return number1 + number2
+
+result=reduce(addition,[10, 20, 30, 40 , 50, 60, 70, 80, 90, 100])
+print(result)
