f7cd59a8b296353931bbb2236410051e56dd24ea
Ganesh Pujari <ganesh.pujari@CALSOFTHQ.COM>
2018-08-07 09:44:20 +0530

    Generator comments fixess

--- scripts/generators.py
+++ scripts/generators.py
@@ -4,8 +4,6 @@ Generators looks like normal fuunction just replace return with yield.
 __next__() method used to fetch elements from generator object.
 Generator remember last fetch elements. So each __next__()call it return next element.
 When no element in generator object it raise StopIterationException.
-
-
 '''
 
 def reverse_data(data):
@@ -23,19 +21,19 @@ print(next(rev_data))
 print(next(rev_data))
 # print(next(rev_data))  StopIteration
 
-#Generator Expressions
+# Generator Expressions
 '''
 We can create generator with expresion.
 simple replace [] with () in list comprahansions.
 '''
 
 gen_obj=(i**2 for i in range(5))
-print(gen_obj.__next__())#0
-print(gen_obj.__next__())#1
-print(gen_obj.__next__())#4
-print(gen_obj.__next__())#9
-print(gen_obj.__next__())#16
-# print(genObj.__next__())#StopIteration
+print(gen_obj.__next__())# 0
+print(gen_obj.__next__())# 1
+print(gen_obj.__next__())# 4
+print(gen_obj.__next__())# 9
+print(gen_obj.__next__())# 16
+# print(genObj.__next__())# StopIteration
 
 gen_obj=(i**3 for i in range(5))
 
